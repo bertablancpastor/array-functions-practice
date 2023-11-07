f7ca7a5107d54e4ca42d370db83f21f1883611da
bertablancpastor <bertablanc89@gmail.com>
2023-11-07 10:16:32 +0100

    done

--- ex1-filter-withdraws.js
+++ ex1-filter-withdraws.js
@@ -6,20 +6,27 @@
  * @param {array} movements Array de numbers que representa entradas y retiradas de dinero
  */
 function filterWithdraws(movements) {
-  // TODO: Implementame
+  return movements.filter((n) => n <= 0);
 }
+// TODO: Implementame
 
 // Caso de prueba 1: Array con varios movimientos de entrada y retirada.
 const test1 = [100, -50, 20, -30, 10];
+//const prueba1 = test1.filter(filterWithdraws);
+//console.log("Caso de prueba 1:", prueba1);
 console.log("Caso de prueba 1:", filterWithdraws(test1));
 // Se espera que el resultado sea [-50, -30].
 
 // Caso de prueba 2: Array con solo movimientos de entrada.
 const test2 = [200, 50, 100, 300];
+//const prueba2 = test2.filter(filterWithdraws);
+//console.log("Caso de prueba 2:", prueba2);
 console.log("Caso de prueba 2:", filterWithdraws(test2));
 // Se espera que el resultado sea [] (ninguna retirada).
 
 // Caso de prueba 3: Array vacío.
 const test3 = [66, -11];
+//const prueba3 = test3.filter(filterWithdraws);
+//console.log("Caso de prueba 3:", prueba3);
 console.log("Caso de prueba 3:", filterWithdraws(test3));
 // Se espera que el resultado sea [-11] .
--- ex2-map-withdraws.js
+++ ex2-map-withdraws.js
@@ -6,19 +6,29 @@
  *
  * @param {array} movements Array de numbers de movimientos bancarios
  */
-function bankStatement(movements) {}
+function bankStatement(movements) {
+  return movements.map((n) => (n >= 0 ? `Deposit: ${n}` : `Withdraw: ${n}`));
+
+  //return movements.map((n) => `Movements ${n}`);
+}
 
 // Caso de prueba 1: Array con varios movimientos de entrada y retirada.
 const test1 = [100, -50, 20, -30, 10];
+//const prueba1 = test1.map(bankStatement);
+//console.log(prueba1);
 console.log("Caso de prueba 1:", bankStatement(test1));
 // Se espera que el resultado sea ["Deposit: 100", "Withdraw: 50", "Deposit: 20", "Withdraw: 30", "Deposit: 10"].
 
 // Caso de prueba 2: Array con solo movimientos de entrada.
 const test2 = [200, 50, 100, 300];
+//const prueba2 = test2.map(bankStatement);
+//console.log(prueba2);
 console.log("Caso de prueba 2:", bankStatement(test2));
 // Se espera que el resultado sea ["Deposit: 200", "Deposit: 50", "Deposit: 100", "Deposit: 300"].
 
 // Caso de prueba 3: Array vacío.
 const test3 = [];
+//const prueba3 = test3.map(bankStatement);
+//console.log(prueba3);
 console.log("Caso de prueba 3:", bankStatement(test3));
 // Se espera que el resultado sea [] (ningún movimiento en el array).
